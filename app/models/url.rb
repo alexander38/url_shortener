ce53f23c78d309c74dfc44de490694c89a4611a3
Alex teo <alexanderteo38@gmail.com>
2017-03-20 07:10:12 +0000

    i controllers updated models updated all views file updated routes.rb updated

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,12 @@
 class Url < ApplicationRecord
+    before_create :shorten
+	# This is Sinatra! Remember to create a migration!
+  validates :long_url,
+  		:uniqueness => true,
+  		:presence => true
+  		# :format => { :with => /http(s)*:\D\D\w+(.)\w+(.\D)*/}
+  	def shorten
+  		# write a method here
+  		self.short_url = SecureRandom.hex(4)
+  	end
 end
